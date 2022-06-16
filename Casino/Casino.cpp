2f8b91bef517e0b6c30ab8d1a6b4de857978c83a
Raikirin <91314577+RodriguesDylan@users.noreply.github.com>
2022-06-16 12:06:04 +0200

    Game loop, removed rng.

--- Casino/Casino.cpp
+++ Casino/Casino.cpp
@@ -2,73 +2,43 @@
 
 // headers
 #include "casino.hpp"
-#include <cstdlib>
-#include <ctime>
-#include <string>
+#include "SlotMachine.hpp"
+#include "randomNumberGuesser.hpp"
 
 // Casino methods
 void Casino::Welcome() { cout << "Welcome to our Casino!\n"; }
 
 int Casino::GameSelect() {
-  int chosenGame = 0;
+  int chosenGame = 0; // game identifier
   cout << "Select a game by entering a number :\n"
-       << "1 : RandomNumberGuesser"
+       << "1 : RandomNumberGuesser      2 : SlotMachine"
        << "\n"
        << "0 : Leave the casino.\n";
   do {
     cin >> chosenGame;
-    if (chosenGame < 0 || chosenGame > 1) {
+    if (chosenGame < 0 || chosenGame > 2) {
       cout << "Must be a valid number!\n";
     }
-  } while (chosenGame < 0 || chosenGame > 1);
+  } while (chosenGame < 0 || chosenGame > 2);
 
   return chosenGame;
 }
 
 void Casino::SelectedGameProcess(int gameNumber) {
+  RandomNumberGuesser rng;
+  SlotMachine sm;
   switch (gameNumber) {
   case 0:
     cout << "Goodbye!\n";
     break;
   case 1:
-    RandomNumberGuesser rng;
     rng.StartMessage();
     rng.Rules();
     rng.GameRNG();
     break;
+  case 2:
+    sm.GameLoop(sm);
+    break;
   }
 }
 
-// RandomNumberGuesser methods
-void RandomNumberGuesser::StartMessage() {
-  cout << "Starting RandomNumberGuesser !\n";
-}
-void RandomNumberGuesser::Rules() {
-  cout << "The rules are :\n"
-       << "    1. Choose a number between 0 and 10.\n"
-       << "    2. If you guessed right, you win!\n";
-}
-void RandomNumberGuesser::GameRNG() {
-  srand(time(nullptr));
-  int number = rand() % 10;
-  int guessedNumber;
-
-  // validity loop
-  do {
-    cout << "Choose a number between 0 and 10 : \n";
-    cin >> guessedNumber;
-
-    if (guessedNumber > 10 || guessedNumber < 0) {
-      cout << "Must be between 0 and 10 ! \n";
-    }
-
-  } while (guessedNumber > 10 || guessedNumber < 0);
-
-  // result
-  if (guessedNumber == number) {
-    cout << "You win!\n";
-  } else {
-    cout << "You lose...\n"
-         << "The number was " << number << "!\n";
-  }
-}
