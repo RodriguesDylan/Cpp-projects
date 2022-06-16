fceaf3906b5b7509f44e0c53a35e67ea120ec012
Raikirin <91314577+RodriguesDylan@users.noreply.github.com>
2022-06-16 12:09:59 +0200

    Create randomNumberGuesser.hpp

---
+++ Casino/randomNumberGuesser.hpp
@@ -0,0 +1,17 @@
+//guard
+#ifndef RNG_H_
+#define RNG_H_
+//header
+#include "casino.hpp"
+
+class RandomNumberGuesser {
+public:
+  RandomNumberGuesser(){};
+  ~RandomNumberGuesser(){};
+
+  void StartMessage();
+  void Rules();
+  void GameRNG();
+};
+
+#endif
