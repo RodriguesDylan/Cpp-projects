ef1e9cf6e2818ed056473fd369f1231ecb60a195
Dylan <dylan.rodriguestop@gmail.com>
2022-06-23 15:23:15 -0400

    Revert "Added username, balance getter."
    This reverts commit f349094fc9157d48c93f2926c3ab1433bcb7eac8.

--- Casino/casino.hpp
+++ Casino/casino.hpp
@@ -1,27 +1,24 @@
 // guard
-#ifndef DB_H_
-#define DB_H_
-// headers
-#include "casino.hpp"
-// libs
-#include <fstream>
-#include <vector>
+#ifndef CASINO_H_
+#define CASINO_H_
+// libraries
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <string>
 
-class Database {
-  string user;
-  string balance;
+using namespace std;
 
+class Casino {
 public:
-  Database() {}
-  ~Database() {}
+  Casino(){};
+  ~Casino(){};
 
-  string &getUser() { return user; }
-  string &getBalance() { return balance; }
-  string *ProcessLine(string line, string *array);
-  void initDB();
-  void Logindb();
-  void clearDB();
-  bool exists_test(const std::string &name);
+  void Welcome();
+  int GameSelect();
+  void SelectedGameProcess(int gameNumber);
 };
+
+
 
 #endif
